f2dc4bd6e8186733cb1cac035d087a6b8e71a9c4
Casey Johnson <caseyjohnsonwv@gmail.com>
2022-10-19 00:34:43 -0400

    overlay audio on final video

--- src/main.py
+++ src/main.py
@@ -3,9 +3,9 @@ import pathlib
 from random import randint, random, choice
 import moviepy.editor as mpe
 
-DIMENSIONS = (720, 1280)
-THREAD_COUNT = 4
-FPS = 24
+DIMENSIONS = (1080, 1920)
+THREAD_COUNT = 8
+FPS = 30
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-a', '--audio', help='Audio file for video montage.')
@@ -32,7 +32,10 @@ print(f"Selecting clips from {len(clips)} files in {dir.as_posix()}.")
 # select clips
 total_time = 0
 edit_list = []
-while len(clips) > 0 and ((ARGS.max_length is not None and total_time < int(ARGS.max_length)) or (ARGS.max_clips is not None and len(edit_list) < int(ARGS.max_clips))):
+while len(clips) > 0:
+    # stop editing if needed
+    if (ARGS.max_length and total_time >= int(ARGS.max_length)) or (ARGS.max_clips and len(edit_list) >= int(ARGS.max_clips)):
+        break
     # choose clip
     if ARGS.randomize:
         clip = choice(clips)
@@ -63,6 +66,13 @@ for clip, start_ts, end_ts in edit_list:
     f = mpe.VideoFileClip(filename=clip)
     new_f = f.resize(DIMENSIONS).subclip(start_ts, end_ts)
     final_clips.append(new_f)
-output = mpe.concatenate_videoclips(final_clips, method='compose')
+output = mpe.concatenate_videoclips(final_clips)
 out_path = pathlib.Path(ARGS.output).joinpath('output.mp4').as_posix()
+
+print(f"Overwriting audio of output file.")
+
+f = mpe.AudioFileClip(filename=ARGS.audio)
+audio_clip = f.subclip(0, output.duration)
+output = output.set_audio(audio_clip)
+assert isinstance(output, mpe.VideoClip)
 output.write_videofile(out_path, threads=THREAD_COUNT, fps=FPS)
